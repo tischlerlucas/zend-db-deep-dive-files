eb19b2a79379d2e9068e59ca5977e6e13b906201
Lucas Tischler <lucas.tischler@vios.com.br>
2018-01-12 09:43:04 -0200

    Criados os scripts que usam predicates

---
+++ reusable-predicates.php
@@ -0,0 +1,50 @@
+<?php
+
+use Db\Services\SqlFactory;
+use Zend\Db\Sql\Expression;
+use Zend\Db\Sql\Predicate\Predicate;
+
+require 'vendor/autoload.php';
+
+$sql = (new SqlFactory())->create();
+
+$populationPredicate = new Predicate();
+$populationPredicate->greaterThanOrEqualTo('population', '?');
+
+$namePredicate = new Predicate();
+$namePredicate->like('ca.name', '?');
+
+$predicate = new Predicate();
+$predicate->addPredicates([
+    $populationPredicate,
+    $namePredicate,
+]);
+
+$select = $sql->select()
+    ->from(['ca' => 'capital'])
+    ->columns([
+        'capital' => 'name',
+        'population',
+    ])
+    ->join(
+        [
+            'ma' => 'mayor'
+        ],
+        'ma.capital_id = ca.id',
+        [
+            'mayor' => 'name',
+            'salary' => new Expression('sum(salary)')
+        ]
+    )
+    ->where($predicate);
+
+$statement = $sql->prepareStatementForSqlObject($select);
+$results = $statement->execute([
+    100000,
+    'Can%',
+]);
+
+//var_dump($results);exit;
+foreach ($results as $result) {
+    printf("%s, %s, %s \n", $result['capital'], $result['population'], $result['mayor']);
+}
